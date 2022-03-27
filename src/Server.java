351897186ec49db2765a3765f467c61153c387a9
Topi6 <csabaschulmann@gmail.com>
2022-03-27 19:08:42 +0200

    Added server and client

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s =  ss.accept();
+        System.out.println("Client conneted");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("client said: " + str);
+        System.out.println("For GIT!");
+    }
+}
